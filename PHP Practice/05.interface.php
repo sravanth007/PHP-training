583d8fe1d3095f29eb13c40fcaa7c1e89946fe28
sravanth007 <90679731+sravanth007@users.noreply.github.com>
2022-07-21 18:27:54 +0530

    Update 05.interface.php

--- PHP Practice/05.interface.php
+++ PHP Practice/05.interface.php
@@ -1,1 +1,17 @@
-
+<?php
+interface A {
+    public function setProperty($x);
+    public function description();
+}
+class Mangoes implements A {
+   public function setProperty($x) {
+        $this->x = $x;
+    }
+    public function description() {
+        echo 'Describing' . $this->x . tree;
+  }
+}
+$Mango = new Mangoes();
+$Mango->setProperty(mango);
+$Mango->description();
+?>
